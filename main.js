388d543f53dcc426abab4db0c2f2923b1c60abe7
LucasAraldi-Dev <lucasaraldi.dev@gmail.com>
2022-12-14 13:19:25 -0800

    Efetuado todas as alterações necessárias para ir para o Vercel

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-const form = document.getElementById('form-atividade')
-const imgAprovado = '<img src="./src/images/aprovado.png" alt="emoji celebrando" />'
-const imgReprovado = '<img src="./src/images/reprovado.png" alt="emoji triste" />'
-const atividades = []
-const notas = []
-const spanAprovado = '<span class = resultado-aprovado>Aprovado</span>'
-const spanReprovado = '<span class = resultado-reprovado>Reprovado</span>'
-const notaMinima = parseFloat(prompt("Digite a Nota minima"))
+const form = document.getElementById('form-agenda')
+const contatos = []
+const numeros = []
 let linhas = ''
 
 
@@ -13,45 +8,29 @@ form.addEventListener('submit', function (e) {
     e.preventDefault()
     adicionarLinha()
     atualizarTabela()
-    atualizarMediaFinal()
+
 })
 function adicionarLinha() {
-    const inputNomeAtividade = document.getElementById('nome-atividade')
-    const inputNotaAtividade = document.getElementById('nota-atividade')
-    
-    if (atividades.includes(inputNomeAtividade.value)) {
-        alert(`A Atividade : ${inputNomeAtividade.value} já foi inserida`)
+    const inputNomeContato = document.getElementById('nome-contato')
+    const inputNumeroContato = document.getElementById('numero-contato')
+
+    if (contatos.includes(inputNomeContato.value)) {
+        alert(`A Pessoa : ${inputNomeContato.value} já foi inserida`)
     } else {
-        atividades.push(inputNomeAtividade.value)
-        notas.push(parseFloat(inputNotaAtividade.value))
+        contatos.push(inputNomeContato.value)
+        numeros.push(parseInt(inputNumeroContato.value))
 
         let linha = '<tr>'
-        linha += `<td>${inputNomeAtividade.value}</td>`
-        linha += `<td>${inputNotaAtividade.value}</td>`
-        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`
+        linha += `<td>${inputNomeContato.value}</td>`
+        linha += `<td>${inputNumeroContato.value}</td>`
         linha += `</tr>`
 
         linhas += linha
     }
-    inputNomeAtividade.value = ''
-    inputNotaAtividade.value = ''
+    inputNomeContato.value = ''
+    inputNumeroContato.value = ''
 }
 function atualizarTabela() {
     const corpoTabela = document.querySelector('tbody')
     corpoTabela.innerHTML = linhas
-}
-function atualizarMediaFinal() {
-    const mediaFinal = calculaMediaFinal()
-    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2)
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado
-
-}
-function calculaMediaFinal() {
-    let somaDasNotas = 0
-
-    for (let i = 0; i < notas.length; i++) {
-        somaDasNotas += notas[i]
-    }
-    return somaDasNotas / notas.length
-
 }
